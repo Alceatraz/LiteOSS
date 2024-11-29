19fb3ae87393a10be0f90c18aa80f0b990a59745
Alceatraz <millerx@live.com>
2024-11-29 14:10:44 +0800

    pipeline-test-001
    Signed-off-by: Alceatraz <millerx@live.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,19 +8,16 @@ group = "top.btswork"
 version = "1.0.0-SNAPSHOT"
 
 plugins {
-  id("io.ktor.plugin") version "2.3.12"
+  id("io.ktor.plugin") version "3.0.1"
   id("org.jetbrains.kotlin.jvm") version "2.1.0"
-//  id("org.graalvm.buildtools.native") version "0.9.28"
   id("org.graalvm.buildtools.native") version "0.10.3"
 }
 
 dependencies {
   implementation("io.ktor:ktor-server-core")
   implementation("io.ktor:ktor-server-cio")
-  implementation("ch.qos.logback:logback-classic:$logbackVersion")
-  testImplementation("io.ktor:ktor-server-tests")
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
-  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+  implementation("org.slf4j:slf4j-simple:2.0.16")
+  // implementation("ch.qos.logback:logback-classic:$logbackVersion")
 }
 
 application {
@@ -50,22 +47,6 @@ graalvmNative {
       buildArgs.add("-H:+ReportUnsupportedElementsAtRuntime")
       imageName.set("liteoss")
     }
-
-    named("test") {
-      verbose.set(true)
-      buildArgs.add("--initialize-at-build-time=io.ktor,kotlin")
-      buildArgs.add("--initialize-at-build-time=ch.qos.logback")
-      buildArgs.add("--initialize-at-build-time=org.slf4j.LoggerFactory")
-      buildArgs.add("--initialize-at-build-time=org.slf4j.helpers.Reporter")
-      buildArgs.add("-H:+InstallExitHandlers")
-      buildArgs.add("-H:+ReportExceptionStackTraces")
-      buildArgs.add("-H:+ReportUnsupportedElementsAtRuntime")
-      val path = "${projectDir}/src/test/resources/META-INF/native-image/"
-      buildArgs.add("-H:ResourceConfigurationFiles=${path}resource-config.json")
-      buildArgs.add("-H:ReflectionConfigurationFiles=${path}reflect-config.json")
-      imageName.set("liteoss-test")
-    }
-
   }
 
   tasks.withType<Test>().configureEach {
